b097f94cfa9507437b8a366723e5d4585f53f5de
tiagoluzh607 <tiagoluz.h607@gmail.com>
2019-06-06 10:33:31 -0300

    Code - Criando Aluno com enums

---
+++ src/main/java/br/com/caelum/vraptor/model/Aluno.java
@@ -0,0 +1,11 @@
+package br.com.caelum.vraptor.model;
+
+public class Aluno {
+	private int id;
+	private String nome;
+	private String email;
+	private String rg;
+	private String telefone;
+	private ESexo sexo;
+	private String cpf;
+}
---
+++ src/main/java/br/com/caelum/vraptor/model/ESexo.java
@@ -0,0 +1,6 @@
+package br.com.caelum.vraptor.model;
+
+public enum ESexo {
+	MASCULINO,
+	FEMININO
+}
